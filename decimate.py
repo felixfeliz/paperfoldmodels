fb3b37e7c3ebf7084c8156bee06d7c4fc9d0ef7a
felixfeliz <felix.scholz@ricam.oeaw.ac.at>
2019-02-19 12:54:48 +0100

    decimate

--- decimate.py
+++ decimate.py
@@ -5,7 +5,7 @@ import numpy as np
 
 #om.read_trimesh(mesh, "models/icosahedron.obj")
 
-mesh = om.read_trimesh('models/bunny')
+mesh = om.read_trimesh('models/bunny.stl')
 
 mesh.triangulate()
 
@@ -18,18 +18,17 @@ for face in mesh.faces():
 print ("No. triangles: " + str(number))
 
 decimater = om.TriMeshDecimater(mesh)
-moduleHandle = om.TriMeshModAspectRatioHandle()
 moduleHandleQuad = om.TriMeshModQuadricHandle()
 #moduleHandle = om.TriMeshModEdgeLengthHandle()
 
-decimater.add(moduleHandle)
+#decimater.add(moduleHandle)
 decimater.add(moduleHandleQuad)
 
 
 #om.TriMeshModEdgeLength.set_edge_length()
 #decimater.module(moduleHandle).set_edge_length(3.5)
-decimater.module(moduleHandle).set_binary(True)
-decimater.module(moduleHandleQuad).set_binary(True)
+#decimater.module(moduleHandle).set_binary(True)
+decimater.module(moduleHandleQuad).set_binary(False)
 
 #print(decimater.module(moduleHandle).is_binary())
 #decimater.module(moduleHandle).set_aspect_ratio(0.3)
@@ -39,11 +38,9 @@ decimater.module(moduleHandleQuad).set_binary(True)
 
 decimater.initialize()
 print(decimater.is_initialized())
-a = decimater.decimate(1500)
+a = decimater.decimate_to_faces(0,120)
 print(a)
 mesh.garbage_collection()
 om.write_mesh('reduced.obj',mesh)
-number = 0
-for face in mesh.faces():
-    number = number + 1
-print ("No. triangles: " + str(number))
+
+print ("No. triangles: " + str(mesh.n_faces()))
